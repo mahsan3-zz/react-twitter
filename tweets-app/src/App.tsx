1f2707f719986a6f154d85a1b8f247e0b4b75168
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-06-06 13:15:12 -0400

    Using antd layoutfor side menu and content area

--- tweets-app/src/App.tsx
+++ tweets-app/src/App.tsx
@@ -1,18 +1,53 @@
 import React from 'react';
-import {Provider} from 'react-redux';
 import './App.css';
-import configureStore from "./store/configureStore";
 import TweetsShell from "./Tweets/containers/TweetsShell";
+import {Layout, Menu} from "antd";
+import { UserOutlined } from '@ant-design/icons';
+const { Header, Content, Footer, Sider } = Layout;
 
-const store = configureStore();
+interface ITwitterAccounts {
+    name: string;
+    screenName: string;
+}
 
 function App() {
+
+  const twitterAccounts: ITwitterAccounts[] = [{
+      name: 'Donald Trump',
+      screenName: 'realDonaldTrump'
+  }, {
+      name: 'Hillary Clinton',
+      screenName: 'HillaryClinton'
+  }];
+
   return (
-    <>
-      <Provider store={store}>
-        <TweetsShell></TweetsShell>
-      </Provider>
-    </>
+      <Layout>
+          <Sider
+              breakpoint="lg"
+              collapsedWidth="0"
+              onBreakpoint={broken => {
+                  console.log(broken);
+              }}
+              onCollapse={(collapsed, type) => {
+                  console.log(collapsed, type);
+              }}
+          >
+              <div className="logo" />
+              <Menu theme="dark" mode="inline" defaultSelectedKeys={['realDonaldTrump']}>
+                  {twitterAccounts.map((acc: ITwitterAccounts, i: number) => (
+                      <Menu.Item key={acc.screenName} icon={<UserOutlined />}>
+                          {acc.name}
+                      </Menu.Item>
+                  ))}
+              </Menu>
+          </Sider>
+          <Layout>
+              <Content style={{ margin: '24px 16px 0' }}>
+                  <TweetsShell></TweetsShell>
+              </Content>
+              <Footer style={{ textAlign: 'center' }}>Ant Design ©2020 Created by Ant UED</Footer>
+          </Layout>
+      </Layout>
   );
 }
 
