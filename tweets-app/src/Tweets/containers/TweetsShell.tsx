605f18483e39f8bd5cbd6428e1ca76ad27f2d871
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-06-06 15:23:34 -0400

    Added helper methods for formatting tweets and user info from state. And removed ul for tweet cards.

--- tweets-app/src/Tweets/containers/TweetsShell.tsx
+++ tweets-app/src/Tweets/containers/TweetsShell.tsx
@@ -1,44 +1,78 @@
 import React, {useEffect} from 'react';
 import {loadTweets} from "../../store/actions/tweets.actions";
 import {connect, DispatchProp, MapDispatchToProps} from "react-redux";
+import {Divider, Spin} from "antd";
+import TweetCard, { ITweetCard } from "../components/TweetCard";
+import TwitterUserHeader, {ITwitterUserInfo} from "../components/TwitterUserHeader";
 
 function TweetsShell(props: any) {
-    useEffect(() => {
-        props.loadTweets('realDonaldTrump');
-    }, []);
+    useEffect(() => {}, []);
 
     if(props.loadingTweets) {
         return (
-            <h2>Loading...</h2>
+            <Spin size="large" />
         );
     }else {
         return (
             <div>
-                <h1>Hello {props.screenName}</h1>
-                <ul>
-                    {props.tweets.map((t: any) => <li>{t.text}</li>)}
-                </ul>
+                { props.accountInfo ? <TwitterUserHeader {...props.accountInfo} /> : null }
+                <Divider>Latest Tweets</Divider>
+                {props.tweets.map((t: ITweetCard, i: number) => (
+                    <TweetCard key={i} {...t} />
+                ))}
             </div>
 
         );
     }
 }
 
-function mapStateToProps(state: any) {
+function formatTweets(t: any): ITweetCard {
     return {
-        tweets: state.tweets.selectedTwitterAccount ? state.tweets.tweets[state.tweets.selectedTwitterAccount] : [],
-        screenName: state.tweets.selectedTwitterAccount,
-        loadingTweets: state.tweets.isLoading
+        text: t.text,
+        createdAt: t.created_at,
+        tweetLink: t.entities.urls.url,
+        retweetCount: t.retweet_count,
+        favoriteCount: t.favorite_count
+    };
+}
+
+function getTweetsForAccount(state: any) {
+    if(state.tweets.selectedTwitterAccount && state.tweets.tweets[state.tweets.selectedTwitterAccount]) {
+        return state.tweets.tweets[state.tweets.selectedTwitterAccount].map((t: any) => formatTweets(t));
+    }else {
+        return [];
     }
 }
 
+function getTwitterAccountInfo(state: any): ITwitterUserInfo | null {
+    const tweets = state.tweets.selectedTwitterAccount && state.tweets.tweets[state.tweets.selectedTwitterAccount];
+    if(tweets && tweets.length >= 1) {
+        const userInfo = tweets[0].user;
+        return {
+            name: userInfo.name,
+            screenName: userInfo.screen_name,
+            description: userInfo.description,
+            followersCount: userInfo.followers_count,
+            profileImage: userInfo.profile_image_url
+        }
+    }else {
+        return null;
+    }
+}
+
+function mapStateToProps(state: any) {
+    return {
+        tweets: getTweetsForAccount(state),
+        screenName: state.tweets.selectedTwitterAccount,
+        loadingTweets: state.tweets.isLoading,
+        accountInfo: getTwitterAccountInfo(state)
+    }
+}
 
 function mapDispatchToProps(dispatch: MapDispatchToProps<any, any>) {
-
     return {
       loadTweets: (screenName: string) => dispatch(loadTweets(screenName))
     };
-
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TweetsShell);
