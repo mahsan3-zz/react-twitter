c122f8b80ff85fd3bbe004ea416f434be5087288
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-06-06 12:01:08 -0400

    Created simple saga for the side effect of getting tweets

---
+++ tweets-app/src/store/sagas/tweets.saga.ts
@@ -0,0 +1,16 @@
+import { call, put, takeLatest } from 'redux-saga/effects'
+import {LOAD_TWEETS, LOAD_TWEETS_FAIL, LOAD_TWEETS_SUCCESS} from "../actions/tweets.actions";
+import {userTweetsApi} from "../api/user-tweets.api";
+
+function* getTweets(action: any) {
+    try {
+        const tweets = yield call(userTweetsApi, action.payload);
+        yield put({type: LOAD_TWEETS_SUCCESS, payload: tweets});
+    } catch (e) {
+        yield put({type: LOAD_TWEETS_FAIL, payload: 'Error'});
+    }
+}
+
+export default  function* loadTweetsSaga() {
+    yield takeLatest(LOAD_TWEETS, getTweets);
+}
