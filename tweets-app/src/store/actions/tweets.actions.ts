e3b39f33ed1ce54f0e68166e779269315fea6013
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-06-06 12:01:58 -0400

    Added actions for loading tweets

---
+++ tweets-app/src/store/actions/tweets.actions.ts
@@ -0,0 +1,24 @@
+import {Action} from "redux";
+
+export const LOAD_TWEETS = 'LOAD_TWEETS';
+export const LOAD_TWEETS_SUCCESS = 'LOAD_TWEETS_SUCCESS';
+export const LOAD_TWEETS_FAIL = 'LOAD_TWEETS_FAIL';
+
+/**
+ * =====================================================
+ *                      LOAD TWEETS
+ * =====================================================
+ */
+export const loadTweets = (screenName: string) => ({
+    type: LOAD_TWEETS,
+    payload: screenName
+});
+
+export const loadTweetsSuccess = (tweets: any) => ({
+    type: LOAD_TWEETS_SUCCESS,
+    payload: tweets
+});
+
+export const loadTweetsFail = () => ({
+    type: LOAD_TWEETS_FAIL
+});
